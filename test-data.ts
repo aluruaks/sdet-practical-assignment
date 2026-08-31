a69cdd22471b3e82f6822d817f99e4df6b84350e
aluruaks <akshayaluru@gmail.com>
2026-08-31 21:28:41 +0530

    Add test data for suggestion validation

---
+++ test-data.ts
@@ -0,0 +1,12 @@
+export const testData = {
+  validSuggestion: 'agile methodology',
+  prefix: 'agile',
+  nonMatchingPrefix: 'xyz',
+  substring: 'method',
+  invalidValue: 'not-a-valid-suggestion',
+  suggestions: [
+    'agile methodology',
+    'agile methodology process',
+    'agile methodology process testing',
+  ],
+};
